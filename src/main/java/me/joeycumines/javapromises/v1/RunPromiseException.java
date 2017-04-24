11be10dad793a7c194a2ba077c28ee08c69c991a
joeycumines <joeycumines@gmail.com>
2017-04-25 01:04:14 +1000

    make the run promise exception just for PromiseRunnable

--- src/main/java/me/joeycumines/javapromises/v1/RunPromiseException.java
+++ src/main/java/me/joeycumines/javapromises/v1/RunPromiseException.java
@@ -1,20 +1,18 @@
 package me.joeycumines.javapromises.v1;
 
-import me.joeycumines.javapromises.core.Promise;
-
 /**
  * Thrown if there was an error running a promise.
  */
 public class RunPromiseException extends RuntimeException {
-    private Promise promise;
+    private PromiseRunnable promise;
 
-    RunPromiseException(Promise promise, String message) {
+    RunPromiseException(PromiseRunnable promise, String message) {
         super("[runtime exception] " + message + ": " + promise.toString());
 
         this.promise = promise;
     }
 
-    public Promise getPromise() {
+    public PromiseRunnable getPromise() {
         return promise;
     }
 }
