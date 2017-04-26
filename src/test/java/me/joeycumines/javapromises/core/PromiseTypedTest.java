0c71fce72ee7253a9bc48376b6df137b14cf6c27
joeycumines <joeycumines@gmail.com>
2017-04-27 02:07:54 +1000

    all tests done! for now anyway

--- src/test/java/me/joeycumines/javapromises/core/PromiseTypedTest.java
+++ src/test/java/me/joeycumines/javapromises/core/PromiseTypedTest.java
@@ -2,8 +2,6 @@ package me.joeycumines.javapromises.core;
 
 import org.junit.Test;
 
-import java.util.function.BiConsumer;
-
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
@@ -57,5 +55,68 @@ public abstract class PromiseTypedTest {
 
         assertEquals(exception, promise.getValue());
         assertEquals(PromiseState.REJECTED, promise.getState());
+    }
+
+    @Test
+    public void testThen() {
+        @SuppressWarnings("unchecked") PromiseTyped<String> first = (PromiseTyped<String>) this.getFactory().resolve("hello");
+
+        Promise second = first.then((r) -> {
+            // this is unchecked, magical
+            return r + " world";
+        });
+
+        second.sync();
+
+        assertEquals("hello world", second.getValue());
+        assertEquals(PromiseState.FULFILLED, second.getState());
+
+        assertEquals("hello", first.getValue());
+        assertEquals(PromiseState.FULFILLED, first.getState());
+    }
+
+    @Test
+    public void testThenWrongType() {
+        // this bit is fine, works as a normal promise
+        Object object = new Object();
+        @SuppressWarnings("unchecked") PromiseTyped<String> first = (PromiseTyped<String>) this.getFactory().resolve(object);
+
+        assertEquals(PromiseState.FULFILLED, first.getState());
+        assertEquals(object, first.getValue());
+
+        // calling then will result in the promise being resolved with an error within the then (a cast error)
+        Promise second = first.then((r) -> true);
+
+        second.sync();
+
+        assertEquals(PromiseState.REJECTED, second.getState());
+        assertTrue(second.getValue() instanceof ClassCastException);
+    }
+
+    @Test
+    public void testThenSync() {
+        @SuppressWarnings("unchecked") PromiseTyped<String> first = (PromiseTyped<String>) this.getFactory().resolve("hello");
+        String value = first.thenSync();
+        value += " world";
+        assertEquals("hello world", value);
+        assertEquals(PromiseState.FULFILLED, first.getState());
+    }
+
+    @Test
+    public void testThenSyncWrongType() {
+        // this bit is fine, works as a normal promise
+        Object object = new Object();
+        @SuppressWarnings("unchecked") PromiseTyped<String> first = (PromiseTyped<String>) this.getFactory().resolve(object);
+
+        assertEquals(PromiseState.FULFILLED, first.getState());
+        assertEquals(object, first.getValue());
+
+        try {
+            // note, we have to assign the value here or it wont try to cast, apparently
+            String value = first.thenSync();
+            fail();
+        } catch (ClassCastException e) {
+            assertNotNull(e);
+        }
     }
 }
