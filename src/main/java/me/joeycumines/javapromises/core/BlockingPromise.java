f60241c28258300d807c92bb6bd320e342592d94
joeycumines <joeycumines@gmail.com>
2017-05-10 15:04:59 +1000

    dont wait around for the full init of BlockingPromise - defer it until or unless needed

--- src/main/java/me/joeycumines/javapromises/core/BlockingPromise.java
+++ src/main/java/me/joeycumines/javapromises/core/BlockingPromise.java
@@ -5,37 +5,25 @@ import java.util.function.Consumer;
 /**
  * Wraps a {@link Promise} created using {@link PromiseFactory}, so that we can resolve a promise after the fact.
  * <p>
- * On construction it will block the current thread until the promise action is run, so that the {@code fulfill} and
- * {@code reject} methods can be accessed safely.
- * <p>
  * This class is tested as part of {@link PromiseFactory}, in the abstract PromiseFactoryTest class.
  */
 public class BlockingPromise<T> {
     private final Promise<T> promise;
-    private Consumer<? super T> _fulfill;
-    private Consumer<Throwable> _reject;
+    private volatile Consumer<? super T> _fulfill;
+    private volatile Consumer<Throwable> _reject;
     private final Object lock;
 
     public BlockingPromise(PromiseFactory factory) {
-        this.lock = new Object();
+        this.lock = this;
 
-        synchronized (this.lock) {
-            this.promise = factory.create((resolve, reject) -> {
-                synchronized (this.lock) {
-                    this._fulfill = resolve;
-                    this._reject = reject;
+        this.promise = factory.create((fulfill, reject) -> {
+            synchronized (this.lock) {
+                this._fulfill = fulfill;
+                this._reject = reject;
 
-                    this.lock.notifyAll();
-                }
-            });
-
-            while (null == this._fulfill || null == this._reject) {
-                try {
-                    this.lock.wait();
-                } catch (InterruptedException ignored) {
-                }
+                this.lock.notifyAll();
             }
-        }
+        });
     }
 
     public Promise<T> getPromise() {
@@ -43,11 +31,33 @@ public class BlockingPromise<T> {
     }
 
     public Promise<T> fulfill(T value) {
+        if (null == this._fulfill) {
+            synchronized (this.lock) {
+                while (null == this._fulfill) {
+                    try {
+                        this.lock.wait();
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            }
+        }
+
         this._fulfill.accept(value);
         return this.promise;
     }
 
     public Promise<T> reject(Throwable value) {
+        if (null == this._reject) {
+            synchronized (this.lock) {
+                while (null == this._reject) {
+                    try {
+                        this.lock.wait();
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            }
+        }
+
         this._reject.accept(value);
         return this.promise;
     }
