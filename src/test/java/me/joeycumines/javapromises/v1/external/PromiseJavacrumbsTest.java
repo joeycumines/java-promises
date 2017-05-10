4a8bebdbea2e2c82b65bd0e1fd542e96ffc3d5f0
joeycumines <joeycumines@gmail.com>
2017-05-10 11:19:41 +1000

    add tests to ensure that CompletableFuture isnt leaking from other implementations

--- src/test/java/me/joeycumines/javapromises/v1/external/PromiseJavacrumbsTest.java
+++ src/test/java/me/joeycumines/javapromises/v1/external/PromiseJavacrumbsTest.java
@@ -1,11 +1,48 @@
 package me.joeycumines.javapromises.v1.external;
 
+import me.joeycumines.javapromises.core.Promise;
 import me.joeycumines.javapromises.core.PromiseFactory;
+import me.joeycumines.javapromises.core.PromiseState;
 import me.joeycumines.javapromises.core.PromiseTest;
+import me.joeycumines.javapromises.v1.PromiseStage;
+import me.joeycumines.javapromises.v1.PromiseStageFactory;
+import net.javacrumbs.completionstage.CompletableCompletionStage;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class PromiseJavacrumbsTest extends PromiseTest {
     @Override
     protected PromiseFactory getFactory() {
         return PromiseJavacrumbsFactory.getInstance();
+    }
+
+    @Test
+    public void testCompletionStagesDontLeakCompletableFutures() {
+        Promise<Object> promise = this.getFactory().fulfill(1);
+
+        if (!(promise instanceof PromiseStage)) {
+            fail();
+        }
+
+        PromiseStage<Object> promiseStage = (PromiseStage<Object>) promise;
+
+        assertTrue(promiseStage.getStage() instanceof CompletableCompletionStage);
+
+        promise = promiseStage.then((value) -> PromiseStageFactory.getInstance().create((fulfill, reject) -> {
+            fulfill.accept((Integer) value + 3);
+        }));
+
+        if (!(promise instanceof PromiseStage)) {
+            fail();
+        }
+
+        promiseStage = (PromiseStage<Object>) promise;
+
+        assertTrue(promiseStage.getStage() instanceof CompletableCompletionStage);
+
+        assertEquals(4, promiseStage.thenSync());
+        assertNull(promiseStage.exceptSync());
+        assertEquals(PromiseState.FULFILLED, promiseStage.getState());
     }
 }
--- src/test/java/me/joeycumines/javapromises/v1/external/PromiseMyFutureTest.java
+++ src/test/java/me/joeycumines/javapromises/v1/external/PromiseMyFutureTest.java
@@ -1,11 +1,49 @@
 package me.joeycumines.javapromises.v1.external;
 
+import me.joeycumines.javapromises.core.Promise;
 import me.joeycumines.javapromises.core.PromiseFactory;
+import me.joeycumines.javapromises.core.PromiseState;
 import me.joeycumines.javapromises.core.PromiseTest;
+import me.joeycumines.javapromises.v1.PromiseStage;
+import me.joeycumines.javapromises.v1.PromiseStageFactory;
+import org.inferred.cjp39.j8stages.MyFuture;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PromiseMyFutureTest extends PromiseTest {
     @Override
     protected PromiseFactory getFactory() {
         return PromiseMyFutureFactory.getInstance();
+    }
+
+    @Test
+    public void testCompletionStagesDontLeakCompletableFutures() {
+        Promise<Object> promise = this.getFactory().fulfill(1);
+
+        if (!(promise instanceof PromiseStage)) {
+            fail();
+        }
+
+        PromiseStage<Object> promiseStage = (PromiseStage<Object>) promise;
+
+        assertTrue(promiseStage.getStage() instanceof MyFuture);
+
+        promise = promiseStage.then((value) -> PromiseStageFactory.getInstance().create((fulfill, reject) -> {
+            fulfill.accept((Integer) value + 3);
+        }));
+
+        if (!(promise instanceof PromiseStage)) {
+            fail();
+        }
+
+        promiseStage = (PromiseStage<Object>) promise;
+
+        assertTrue(promiseStage.getStage() instanceof MyFuture);
+
+        assertEquals(4, promiseStage.thenSync());
+        assertNull(promiseStage.exceptSync());
+        assertEquals(PromiseState.FULFILLED, promiseStage.getState());
     }
 }
